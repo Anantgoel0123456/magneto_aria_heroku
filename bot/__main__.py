560d464eb5b7fb1cf5689325605e3286797c790c
Anantgoel0123456 <88424558+Anantgoel0123456@users.noreply.github.com>
2021-09-25 08:20:46 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -205,7 +205,7 @@ def main():
     dispatcher.add_handler(help_handler)
     dispatcher.add_handler(stats_handler)
     dispatcher.add_handler(log_handler)
-    updater.start_polling(drop_pending_updates=IGNORE_PENDING_REQUESTS)
+    updater.start_polling()
     LOGGER.info("Bot Started!")
     signal.signal(signal.SIGINT, fs_utils.exit_clean_up)
 app.start()
